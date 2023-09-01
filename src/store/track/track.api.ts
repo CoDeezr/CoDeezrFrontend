79d7b623ceeb1ee8a654a9b55bc2f6aa5b9c1df6
GProDev <gpapadoudou@gmail.com>
2023-09-01 21:56:24 +0000

    Created redux query API to fetch tracks

---
+++ src/store/track/track.api.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { APIResponse } from '../../model/api-response'
+import { Track } from '../../model/track'
+
+const trackApi = createApi({
+  reducerPath: 'trackApi',
+  tagTypes: ['tracks'],
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:3000' }),
+  endpoints: (builder) => ({
+    getTracks: builder.query<APIResponse<Track[]>, { search: string, index: number }>({
+      query: ({ search, index }) => `/search?q=${search}&index=${index}`,
+      providesTags: ['tracks'],
+    })
+  })
+})
+
+export const {
+  useGetTracksQuery,
+} = trackApi
+
+export default trackApi
