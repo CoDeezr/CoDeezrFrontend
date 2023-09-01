42fccfdb8926faafc05f622815c93f65e1715757
GProDev <gpapadoudou@gmail.com>
2023-09-01 22:05:24 +0000

    Updated TrackItem component with information from track passed as prop

--- src/pages/TrackItem.tsx
+++ src/pages/TrackItem.tsx
@@ -1,6 +1,8 @@
-import { Card, Image, Text, Group, Badge, createStyles, rem, Stack } from '@mantine/core'
+import { Card, Image, Text, Group, Badge, createStyles, rem, Stack, Popover } from '@mantine/core'
 import { Link } from 'react-router-dom'
 import { IconLink } from '@tabler/icons-react'
+import { Track } from '../model/track'
+import { useDisclosure } from '@mantine/hooks'
 
 const useStyles = createStyles((theme) => ({
   card: {
@@ -8,7 +10,6 @@ const useStyles = createStyles((theme) => ({
   },
 
   imageSection: {
-    // padding: theme.spacing.md,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -39,22 +40,41 @@ const useStyles = createStyles((theme) => ({
   artistLink: {
     textDecoration: 'none',
     color: theme.colorScheme === 'dark' ? theme.colors.gray[0] : theme.colors.gray[7],
-  }
+  },
+
+  title: {
+    maxWidth: '100%',
+    fontWeight: 500,
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
 }))
 
-const TrackItem = () => {
+const TrackItem = ({ track }: { track: Track }) => {
   const { classes } = useStyles()
+
+  const [opened, { open, close }] = useDisclosure(false)
 
   return (
     <Card withBorder radius="md" className={classes.card}>
       <Card.Section className={classes.imageSection}>
         <Image
-          src="https://e-cdns-images.dzcdn.net/images/cover/ec3c8ed67427064c70f67e5815b74cef/250x250-000000-80-0-0.jpg"
-          alt="Tesla Model S" />
+          src={track.album.cover_medium}
+          alt={track.album.title} />
       </Card.Section>
 
       <Stack mt="md" spacing={0} mb={30} align="start">
-        <Text fw={500}>Curtain Call 2</Text>
+        <Popover width={200} position="bottom" withArrow shadow="md" opened={opened}>
+          <Popover.Target>
+            <Text className={classes.title} onMouseEnter={open} onMouseLeave={close}>
+              {track.title}
+            </Text>
+          </Popover.Target>
+          <Popover.Dropdown sx={{ pointerEvents: 'none' }}>
+            <Text size="sm">{track.title}</Text>
+          </Popover.Dropdown>
+        </Popover>
         <Text color="dimmed" size="sm">Duration</Text>
         <Badge variant="outline">02:40</Badge>
       </Stack>
@@ -68,7 +88,7 @@ const TrackItem = () => {
             <Link to={`/artist/13`} className={classes.artistLink}>
               <Group spacing={5}>
                 <Text fz="xl" fw={700} sx={{ lineHeight: 1 }}>
-                  Burna Boy
+                  {track.artist.name}
                 </Text>
                 <IconLink size={20} />
               </Group>
