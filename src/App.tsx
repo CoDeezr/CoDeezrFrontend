f18d53c5c54719f17f21b8cc6af0cb02872cda68
GProDev <gpapadoudou@gmail.com>
2023-09-01 17:28:56 +0000

    Setup mantine and in app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core'
+import { useLocalStorage } from '@mantine/hooks'
+import { Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import ArtistPage from './pages/ArtistPage'
+import AppHeader from './components/AppHeader'
+
 function App() {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({ key: 'colorScheme', defaultValue: 'light' })
+
+  function toggleColorScheme() {
+    setColorScheme(p => p == 'dark' ? 'light' : 'dark')
+  }
 
   return (
-    <>
-      <h1>CoDeezr</h1>
-    </>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme,
+          primaryColor: 'red',
+        }}>
+        <AppHeader toggleColorScheme={toggleColorScheme} />
+        <Routes>
+          <Route index element={<MainPage />} />
+          <Route path='artist/:id' element={<ArtistPage />} />
+        </Routes>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
