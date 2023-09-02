a50064837dd503b0ddb0712ab2380cbc0fd8deb7
GProDev <gpapadoudou@gmail.com>
2023-09-02 15:32:13 +0000

    Added ArtistDetail, TopTracksList and AlbumsList to ArtistPage

--- src/pages/ArtistPage.tsx
+++ src/pages/ArtistPage.tsx
@@ -1,6 +1,62 @@
+import { createStyles, Button, Center, Container, Grid, Loader, rem } from '@mantine/core'
+import { IconChevronLeft } from '@tabler/icons-react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useGetArtistQuery } from '../store/artist/artist.api'
+import ArtistDetail from '../components/ArtistDetail'
+import TopTracksList from '../components/TopTracksList'
+import AlbumsList from '../components/AlbumsList'
+
+const useStyles = createStyles(theme => ({
+  artistDetail: {
+    borderRadius: theme.radius.lg,
+    border: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]}`,
+    padding: `${theme.spacing.sm} ${theme.spacing.xl}`,
+  },
+  carouselItem: {
+    backgroundColor: theme.primaryColor,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+}))
+
 const ArtistPage = () => {
+  const { classes } = useStyles()
+
+  const { id } = useParams()
+
+  const { data: artist, isFetching } = useGetArtistQuery(parseInt(id!))
+
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1)
+  }
+
   return (
-    <div>ArtistPage</div>
+    <Container>
+      <Button
+        onClick={goBack}
+        variant="light"
+        leftIcon={<IconChevronLeft />}
+        mb={50}>
+        BACK
+      </Button>
+
+      {isFetching && <Center><Loader /></Center>}
+
+      {artist && <Grid gutter={40} align="stretch" p={10}>
+        <Grid.Col sm={12} md={8} className={classes.artistDetail}>
+          <ArtistDetail artist={artist} />
+        </Grid.Col>
+        <Grid.Col sm={12} md={4}>
+          <TopTracksList artist={artist} />
+        </Grid.Col>
+        <Grid.Col sm={12}>
+          <AlbumsList artist={artist} />
+        </Grid.Col>
+      </Grid>}
+    </Container>
   )
 }
 
